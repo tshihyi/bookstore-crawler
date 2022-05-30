3f1c1460c1adbf721515edd530f062218c3653dd
tshihyi <30917474+tshihyi@users.noreply.github.com>
2022-05-30 20:03:14 +0800

    Add date column as PPI
    Add date column as Partition Primary Index
    Add updateData function

--- load_data.py
+++ load_data.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from pymongo import MongoClient
-
 import pymongo
 from urllib.parse import quote_plus
+from pymongo import MongoClient
+from datetime import date
 
+current_date = date.today().isoformat()
 
 # get Best selling from book store
 def get_best_selling_list():
@@ -16,13 +17,15 @@ def get_best_selling_list():
     tags = content.find(class_="mod type02_m035 clearfix")
     tagList = tags.select('.main_wrap .type02_m035 .type02_bd-a')
 
-    #table layout
-    tagDict = {'title': [], 'author': [], 'price': []}
+    #table layout, date as PPI
+    tagDict = {'title': [], 'author': [], 'price': [], 'date':[]}
 
     for t in tagList:
         tagDict['title'].append(t.select('a')[0].text) 
         tagDict['author'].append(t.select('a')[1].text)
         tagDict['price'].append(t.find('b').text)
+        tagDict['price'].append(t.select('.price_a b:last-child')[-1].text)
+        tagDict['date'].append(current_date)
      
     # table column
     df = pd.DataFrame.from_dict(tagDict)
@@ -45,6 +48,10 @@ def insertData():
     apply_count = mycollection.count_documents({})
     print("Insert ", apply_count, " rows success!")
 
+def updateData():
+    mycollection.delete_many({"date": current_date})
+    print("Delete ", total_count, "rows success!")
+
 mongoDBId = mongoDBId
 mongoDBPwd = mongoDBPwd
 connStr = "mongodb+srv://" + mongoDBId + ":" + mongoDBPwd + "@cluster0.fkroy.mongodb.net/sample_analytics?retryWrites=true&w=majority"
@@ -53,10 +60,9 @@ mydb = mongoConnect(mongoDBId, mongoDBPwd, connStr).marketing
 mycollection = mydb.bookstore_best_selling #tablename
 total_count = mycollection.count_documents({})
 
-#Avoid insert duplicate
+#Avoid insert duplicate data / monthly batch
 if total_count > 1:
-    mycollection.drop()
-    print("Delete ", total_count, "rows success!")
+    updateData()
     insertData()
 else: #Data Initial
     insertData()
